877cc30fe9958f4477f9269c426b5e5b3dfa2c14
cshjang <dhjang@conextt.com>
2023-09-25 13:59:28 +0900

    1.Step clear

--- exchange.sv
+++ exchange.sv
@@ -1,13 +1,21 @@
-// exchange
+module a;
 
 function get_Euro_from_Dollar(real amount);
 	real euro;
-	real euro = 0.93871 * amount;
+	euro = 0.93871 * amount;
 	$display("Dollar %f = Euro %f", amount, euro);
 endfunction
 
 function get_Dollar_from_Euro(real amount);
 	real dollar;
-	dollar =amount / 0.93871
-	$display("Euro %f = Dollar %f", amount, euro);
+	dollar =amount / 0.93871;
+    $display("Euro %f = Dollar %f", amount, dollar);
 endfunction
+  
+  initial begin
+    
+    get_Euro_from_Dollar(10);
+    get_Dollar_from_Euro(10);
+  end
+    
+endmodule
